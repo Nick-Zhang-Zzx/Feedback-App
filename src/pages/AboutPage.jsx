151d9c9d00f4acfe1b78bb7f9917cbea79fe8a12
Nick Zhang <nick.zhang@rice.edu>
2022-02-15 00:36:46 -0600

    Edit README

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,6 +8,7 @@ function AboutPage() {
                 <h1>About This Project</h1>
                 <p>This is a React app to leave feedback for a product or service</p>
                 <p>Version: 1.0.0</p>
+                <p>Created by: Nick Zhang</p>
                 <p><Link to="/">Back to home</Link></p>
             </div>
         </Card>
